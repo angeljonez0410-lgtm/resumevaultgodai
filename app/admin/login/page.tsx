863f018e8b4db670b429acc4a99fefd0de85abbd
angeljonez0410 <angeljonez0410@gmail.com>
2026-04-09 21:27:42 -0700

    Force login page dynamic to fix env vars

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,5 +1,7 @@
 import LoginForm from "../../../components/LoginForm";
 
+export const dynamic = "force-dynamic";
+
 export default function AdminLoginPage() {
   return (
     <main className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
